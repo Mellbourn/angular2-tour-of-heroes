317da9a4762afb1f95b032032b89fd586e0dd5e8
Klas Mellbourn <klas@mellbourn.net>
2016-09-06 21:37:27 +0200

    Finished first chapter, "Once Upon a Time" https://angular.io/docs/ts/latest/tutorial/toh-pt1.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,7 +11,7 @@ export class Hero {
     template: `<h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label><input value="{{hero.name}}" placeholder="name"></div>`
+    <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>`
 })
 export class AppComponent {
   title = 'Tour of Heroes';

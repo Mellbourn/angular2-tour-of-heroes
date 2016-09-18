68e7e6d58f73551fe23bdffc2c3022cfc10b23d6
Klas Mellbourn <klas@mellbourn.net>
2016-09-18 22:32:06 +0200

    finished chapter 6, routing: added premade styles

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 @Component({
   selector: 'my-hero-detail',
+  styleUrls: ['app/hero-detail.component.css'],
   templateUrl: 'app/hero-detail.component.html'
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
   constructor(
     private heroService: HeroService,

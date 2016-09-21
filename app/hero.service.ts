4c6b0b05836932376b426d99ed01a8a80e171fb1
Klas Mellbourn <klas@mellbourn.net>
2016-09-21 22:55:32 +0200

    chapter 7: update and create heroes using REST api

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,6 +8,7 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'app/heroes'; // URL to web api
+  private headers = new Headers({ 'Content-Type': 'application/json' });
 
   constructor(private http: Http) { }
 
@@ -20,6 +21,23 @@ export class HeroService {
 
   getHero(id: number): Promise<Hero> {
     return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), { headers: this.headers })
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -12,22 +12,39 @@ export class HeroesComponent implements OnInit {
   hero: Hero = { id: 1, name: 'Windstorm' };
   heroes: Hero[];
   selectedHero: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => {
       this.heroes = heroes;
     });
   }
+  
   ngOnInit(): void {
     this.getHeroes();
   }
+  
   constructor(
     private router: Router,
     private heroService: HeroService
-    ) { }
-    gotoDetail(): void {
-      this.router.navigate(['/detail', this.selectedHero.id]);
+  ) { }
+  
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if(!name) {
+      return;
     }
+    this.heroService.create(name)
+    .then(hero => {
+      this.heroes.push(hero);
+      this.selectedHero = null;
+    })
+  }
 }
